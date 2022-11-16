a0bf695ea5ff87c924838f8db0158392aba265bd
B.E.B.B <larous25@hotmail.es>
2022-11-16 15:46:58 -0500

    se agrego la creacion de todas las combianaciones y eliminacion de las repetidas

--- main.js
+++ main.js
@@ -19,47 +19,63 @@ const getAllSubsets = theArray => theArray.reduce(
 // consigue el array del tablero o sea todas las casillas del trablero
 function conseguirUnaCombinacion (vectorCombinacion, a) {
     let m = JSON.parse(JSON.stringify(a))
-    if(vectorCombinacion.length == 0) {
-        for (let i = 1; i < m.length; i++) {
-            for (let j = 0; j < m[i].length; j++) {
-                m[i][j] = m[i-1][j] + (-1)**(i+1)
-            }
-        }
-    } else {         
+    if(vectorCombinacion.length != 0) {
         vectorCombinacion.map(e => {
-            console.log("elemento de inicio: " +e)
             m[0][e] = 1
         })
-
-        for (let i = 1; i < m.length; i++) {
-            for (let j = 0; j < m[i].length; j++) {
-                m[i][j] = m[i-1][j] + (-1)**(m[i-1][j])
-            }
-        }
     }
+    
+    llenado(m)
         
     return [...[...m]]
     
 }
 
+function llenado(m) {
+    for (let i = 1; i < m.length; i++) {
+        for (let j = 0; j < m[i].length; j++) {
+            m[i][j] = m[i-1][j] + (-1)**(m[i-1][j])
+        }
+    }
+    return m
+}
+
 
 function conseguirTodasLasCombinaciones(partes, a) {
-    let combinaciones = [];
+    let combinaciones = []
 
     for (let indexPotencia = 0; indexPotencia < partes.length; indexPotencia++) {        
         const vectorCombinacion = partes[indexPotencia];
-        console.log("combinacion actual: ", vectorCombinacion);
         combinaciones.push(conseguirUnaCombinacion(vectorCombinacion, a))
     }
 
-    return combinaciones();
+    return combinaciones
 }
 
+function multiDimensionalUnique(arr) {
+    var uniques = [];
+    var itemsFound = {};
+    for(var i = 0, l = arr.length; i < l; i++) {
+        var stringified = JSON.stringify(arr[i]);
+        if(itemsFound[stringified]) {
+            console.log(arr[i])
+            continue; 
+        }
+
+        uniques.push(arr[i]);
+        itemsFound[stringified] = true;
+    }
+    return uniques;
+}
+
+
 // para realizar la transpocición
-// output = array[0].map((_, colIndex) => array.map(row => row[colIndex]));
+function transponerMatrices (combinaciones) {
+    return combinaciones.map(c => c[0].map((_, colIndex) => c.map(row => row[colIndex])))
+}
 
 
-
+// main XD
 function crearTablero () {
     let numeroFilas = parseInt(document.querySelector("#filas").value)
     let numeroColumnas = parseInt(document.querySelector("#columnas").value)
@@ -76,44 +92,48 @@ function crearTablero () {
     // falta calcular transpuesta en caso de que las columnas sean diferentes a las filas
     
     estructurarTablero(numeroFilas, numeroColumnas)
-    let combianacionAPintar = 0
-    pintarCombinacion(conseguirUnaCombinacion(partesFila[combianacionAPintar], a))
+    let combinacionAPintar = 0
+    
+    let combianacionesFila = conseguirTodasLasCombinaciones(partesFila, a)
+    let combinacionesColumna = transponerMatrices(combianacionesFila)
+    
+    let todasLasCombinaciones = multiDimensionalUnique([...combianacionesFila, ...combinacionesColumna])
+    
 
+    pintarCombinacion(todasLasCombinaciones[combinacionAPintar])
+
+    // pinta la respuesta
     document.querySelector('#mensaje').removeAttribute('style')
     document.querySelector('#posibilidades').innerHTML = `${2**numeroFilas + 2**numeroColumnas - 2}`
     let mensajeCombinacionElement = document.querySelector('#combianacion')
 
+
+
     document.querySelector('#anterior').addEventListener('click', () => {
-        combianacionAPintar--;
-        mensajeCombinacionElement.innerHTML = combianacionAPintar 
-        // console.log("se manda a una combinación anterior; ", combianacionAPintar)
-        const vectorCombinacion = partesFila[combianacionAPintar];
-        // console.log(vectorCombinacion)
-        pintarCombinacion(conseguirUnaCombinacion(vectorCombinacion, a))
+        combinacionAPintar--;
+        mensajeCombinacionElement.innerHTML = (combinacionAPintar+1) 
+        pintarCombinacion(todasLasCombinaciones[combinacionAPintar])
     })
 
     document.querySelector('#siguiente').addEventListener('click', () => {
-        combianacionAPintar++;
-        mensajeCombinacionElement.innerHTML = combianacionAPintar
-        // console.log("se manda a una combinación siguiente; ", combianacionAPintar)
-        const vectorCombinacion = partesFila[combianacionAPintar];
-        // console.log(vectorCombinacion)
-        pintarCombinacion(conseguirUnaCombinacion(vectorCombinacion, a))
+        combinacionAPintar++;
+        mensajeCombinacionElement.innerHTML = (combinacionAPintar+1)
+        pintarCombinacion(todasLasCombinaciones[combinacionAPintar])
     })
 }
 
 
-function pintarCombinacion(combianacionAPintar) {
-    console.log(combianacionAPintar)
+function pintarCombinacion(combinacionAPintar) {
+    console.log(combinacionAPintar)
     let divs = document.querySelectorAll("#tablero > div")
     for (let i = 0; i < divs.length; i++) 
         divs[i].removeAttribute('style')
     
 
     let con = 0;
-    for (let i = 0; i < combianacionAPintar.length; i++) {
-        for (let j = 0; j < combianacionAPintar[i].length; j++) {
-            if(combianacionAPintar[i][j] == 1) {
+    for (let i = 0; i < combinacionAPintar.length; i++) {
+        for (let j = 0; j < combinacionAPintar[i].length; j++) {
+            if(combinacionAPintar[i][j] == 1) {
                 let s = ``;
                 divs[con].style.background = '#F24B6A';
             }
